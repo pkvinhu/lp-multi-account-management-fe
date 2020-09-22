b2fc78582e31979648e4edd46536e40124758c93
Kevin Hu <khu@liveperson.com>
2020-09-22 16:41:22 -0400

    create login folder and rendering basic button

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <button>click me</button>
+            </div>
+        )
+    }
+}
