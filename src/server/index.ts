560ec90361a9fc873969860e0b0aa35eb1cd8b0d
Kevin Hu <khu@liveperson.com>
2020-09-25 14:47:44 -0400

    added express server into react app, compiled with webpack, and configured with a separate ts.config file

---
+++ src/server/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+const buildDir = path.join(process.cwd() + "/build");
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(buildDir));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(buildDir, "index.html"));
+});
+
+app.get("/some-file", (req, res) => {
+  res.send("hello world")
+})
+
+const port = 3001;
+console.log("checking port", port);
+app.listen(port, () => {
+  console.log(`Server now listening on port: ${port}`);
+});
